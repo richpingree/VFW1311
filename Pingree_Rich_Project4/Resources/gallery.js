1c3ed2912c671160519fabda19a92d4da74b32f3
richpingree <richpingree@fullsail.edu>
2013-11-21 20:23:50 -0600

    updated code for gallery to get a random image to load, when first opened a random image does appear but then my button stopped working, so commented the code out to get the button to work again

--- Pingree_Rich_Project4/Resources/gallery.js
+++ Pingree_Rich_Project4/Resources/gallery.js
@@ -1,35 +1,65 @@
-var myImages = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, "charimages");
-var imageList = myImages.getDirectoryListing();
-
 var galWin = Ti.UI.createWindow({
 		title: "Member's Gallery",
 		modal: true,
 		backgroundColor: "#595BC2"
 });
+var counter = 1;
+
+var myImages = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, "charimages");
+var imageList = myImages.getDirectoryListing();
 	
-var newPicView = Ti.UI.createView({
-	backgroundColor:"#E80505",
-	height: viewHeight,
-	bottom: 20
+
+var button = Ti.UI.createButton({
+	bottom: 10,
+	title: "Change Image",
+	height: 50,
+	width: 200,
+	backgroundColor: "Red",
+	color: "#000",
+	borderRadius: 25
 });
 
-var newPicLabel = Ti.UI.createLabel({
-	text: "New Picture",
-	color: "#fff",
-	font: {fontSize: 20, fontFamily: "Times New Roman"},
-	textAlign: "center"
-	});
+// var imgList = Array();
+	// for(var i=0; i<imageList.length; i++){
+		// var index = Math.floor(Math.random()*imageList.length);
+		// imgList.push(imageList[index]);
+		// imageList.splice(index,1);
+// };		
+
+
 	
 var photo = Ti.UI.createImageView({
-	image: "charimages/"+ imageList[0],
+	image: "charimages/"+ imageList[counter],
 	top: 10,
-	bottom: (newPicView.bottom + newPicView.height) + 10
+	bottom: (button.bottom + button.height) + 10
 		
 });	
 
+
+// button.addEventListener("click", function(){
+	// if(counter>=imageList.length){
+	// photo.image = "charimages/" + imgList[counter];
+	// counter++;
+// 		
+	// }else{
+		// counter = 0;
+	// }
+// 	
+// });
+
+button.addEventListener("click", function(e){
+	photo.setImage("charimages/" + imageList[counter]);
+	counter++;
+	if(counter>=imageList.length){
+		counter = 0;
+	}
+	
+});
+
+
 galleryView.addEventListener("click", function(e){
 	
-	newPicView.add(newPicLabel);
-	galWin.add(newPicView, photo);
+	galWin.add(photo, button);
 	navWin.openWindow(galWin, {animate: true});
 });
+
